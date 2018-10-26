847ebbc48d0aba303011bfc72d680748eeb4027f
xiongsongsong <xiongsongsong@outlook.com>
2018-10-26 22:26:23 +0800

    增加EOF的判断

--- src/ble-server.js
+++ src/ble-server.js
@@ -11,6 +11,15 @@ const { Subject } = require('rxjs')
 const subject = new Subject()
 const {debounce} = require('lodash')
 
+const EOF_BUF = new ArrayBuffer(20)
+const EOF_BUF_VIEW = new Uint16Array(EOF_BUF)
+EOF_BUF_VIEW.fill(0xff)
+EOF_BUF_VIEW[6] = 0x45 // E
+EOF_BUF_VIEW[7] = 0x4f // O
+EOF_BUF_VIEW[8] = 0x46 // F
+EOF_BUF_VIEW[9] = 0x21 //!
+const EOF = Buffer.from(EOF_BUF)
+
 /**
  * 服务
  * 
@@ -78,8 +87,8 @@ bleno.on('advertisingStart', (error) => {
             },
             onWriteRequest(data, offset, withoutResponse, callback) {
               BLE_BUFFER = Buffer.concat([BLE_BUFFER, data])
-              console.log(Date.now()-prevNow)
-              prevNow = Date.now()
+              //console.log(Date.now()-prevNow)
+              //prevNow = Date.now()
               check()
               callback(bleno.Characteristic.RESULT_SUCCESS)
             }
@@ -90,7 +99,12 @@ bleno.on('advertisingStart', (error) => {
   }
 })
 
-const check = debounce(()=>{
-  console.log(BLE_BUFFER.toString('utf16le') ,BLE_BUFFER.length)
-  BLE_BUFFER = Buffer.from('')
-}, 500)
+function check(){
+  // 判断 BLE_BUFFER 末尾的数据，是否和 EOF_BUF 相等
+  // 如果相等，则说明数据传输完毕
+  let CACHE = BLE_BUFFER.slice(BLE_BUFFER.length - 20)
+  if(CACHE.equals(EOF)){
+    console.log(BLE_BUFFER.slice(0, -20).toString('utf16le'))
+    BLE_BUFFER = Buffer.from('')
+  }
+}
